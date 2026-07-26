603e67dd31826d878003a40542e095b4f035e907
Rofik <berlianindoweb@gmail.com>
2026-07-27 02:21:32 +0700

    Cache gerbang akses internal Kawan Bus

--- kawan-bus/sw.js
+++ kawan-bus/sw.js
@@ -1,5 +1,5 @@
-const CACHE='kawan-bus-pramusapa-v0.8.0';
-const ASSETS=['./','index.html','app.css','app.js','manifest.webmanifest','admin.html','admin.js','reliever.html','reliever.js','panduan.html','pedoman-bus.html','../assets/img/favicon.svg'];
+const CACHE='kawan-bus-pramusapa-v0.9.0';
+const ASSETS=['./','index.html','app.css','app.js','manifest.webmanifest','admin.html','admin.js','reliever.html','reliever.js','panduan.html','pedoman-bus.html','access.html','access-control.js','../assets/img/favicon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{
